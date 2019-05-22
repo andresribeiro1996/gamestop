4905b519583e33f387fa5d347f1826b354690edf
João Oliveira <joao.oliveira@byside.com>
2019-05-22 09:43:44 +0100

    fixes

--- gamestop/screens/sideMenu/HomeScreen.js
+++ gamestop/screens/sideMenu/HomeScreen.js
@@ -1,48 +1,47 @@
-import React, { Component } from 'react'
-import { View, Text, StyleSheet } from 'react-native';
-import {Header, Left, Right, Icon, Body} from 'native-base';
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { Header, Left, Right, Icon, Body } from "native-base";
 
 class HomeScreen extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-    constructor(props) {
-        super(props);
-    }
+  static navigationOptions = {
+    drawerIcon: ({ tintColor }) => (
+      <Icon name="home" style={{ fontSize: 24, color: tintColor }} />
+    )
+  };
 
-    static navigationOptions = {
-        drawerIcon : ({tintColor}) => (
-            <Icon name="home" style={{ fontSize: 24, color: tintColor}}/>
-        )
-    }
-    
-    render(){
-        return(
-            <View style={styles.container}>
-                <Header>
-                    <Left>
-                        <Icon 
-                            name="menu"
-                            onPress={() => this.props.navigation.openDrawer()}
-                        />
-                    </Left>
-                    <Body/>
-                    <Right/>
-                </Header>
+  render() {
+    return (
+      <View style={styles.container}>
+        <Header>
+          <Left>
+            <Icon
+              name="menu"
+              onPress={() => this.props.navigation.openDrawer()}
+            />
+          </Left>
+          <Body />
+          <Right />
+        </Header>
 
-                <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}> 
-                    {/* <Text>{this.props.screenProps.isLoggedIn}</Text> */}
-                    oi
-                </View>
-            </View>
-        );
-    }
+        <View
+          style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+        >
+          {/* <Text>{this.props.screenProps.isLoggedIn}</Text> */}
+          <Text>oi</Text>
+        </View>
+      </View>
+    );
+  }
 }
 
 export default HomeScreen;
 
-const styles = StyleSheet.create(
-    {
-        container:{
-            flex:1,
-        }
-    }
-)
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
